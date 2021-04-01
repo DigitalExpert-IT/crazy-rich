71d35588e3e71b77dba97a330b087589fe62e0d9
Muhammad Rizky Firdaus <62287144+ubudab109@users.noreply.github.com>
2021-04-01 20:57:02 +0700

    Update index.php

--- public_html/apps/index.php
+++ public_html/apps/index.php
@@ -234,9 +234,9 @@ include('template/fungsi.php');
 
 	<!-- Right bar overlay-->
 	<div class="rightbar-overlay"></div>
+	<script src="../minible/libs/node-waves/waves.min.js"></script>
 	<script src="../minible/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
 	<script src="../minible/libs/simplebar/simplebar.min.js"></script>
-	<script src="../minible/libs/node-waves/waves.min.js"></script>
 	<script src="../minible/libs/waypoints/lib/jquery.waypoints.min.js"></script>
 	<script src="../minible/libs/jquery.counterup/jquery.counterup.min.js"></script>
 </body>
