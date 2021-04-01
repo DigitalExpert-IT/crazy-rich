16e21440e59ec4f49e6012df87d9ea85eeac7d0c
Muhammad Rizky Firdaus <62287144+ubudab109@users.noreply.github.com>
2021-04-01 20:37:35 +0700

    waves

--- public_html/apps/template/header.php
+++ public_html/apps/template/header.php
@@ -28,32 +28,23 @@
     <!-- App Css-->
     <link href="../minible/css/app.min.css" id="app-style" rel="stylesheet" type="text/css" />
 
-    <script src="../minible/libs/jquery/jquery.min.js"></script>
+    <!-- Datatable -->
+    <!-- DataTables -->
     <link href="../minible/libs/datatables.net-bs4/css/dataTables.bootstrap4.min.css" rel="stylesheet" type="text/css" />
     <link href="../minible/libs/datatables.net-buttons-bs4/css/buttons.bootstrap4.min.css" rel="stylesheet" type="text/css" />
 
     <!-- Responsive datatable examples -->
     <link href="../minible/libs/datatables.net-responsive-bs4/css/responsive.bootstrap4.min.css" rel="stylesheet" type="text/css" />
-    <script src="../minible/js/custom.js"></script>
-
-    <script>
-        function copyreff() {
-            var copyText = document.getElementById("reflink");
-            copyText.select();
-            copyText.setSelectionRange(0, 99999)
-            document.execCommand("copy");
-            alert("Copied: " + copyText.value);
-        }
-    </script>
+    <!-- JAVASCRIPT -->
+    <script src="../minible/libs/jquery/jquery.min.js"></script>
 </head>
 
 
 <body>
-    <!-- JAVASCRIPT -->
     <script src="../minible/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
     <script src="../minible/libs/metismenu/metisMenu.min.js"></script>
-    <script src="../minible/libs/node-waves/waves.min.js"></script>
     <script src="../minible/libs/simplebar/simplebar.min.js"></script>
+    <script src="../minible/libs/node-waves/waves.min.js"></script>
     <script src="../minible/libs/waypoints/lib/jquery.waypoints.min.js"></script>
     <script src="../minible/libs/jquery.counterup/jquery.counterup.min.js"></script>
 
@@ -96,6 +87,16 @@
 
     <!-- Sweet alert init js-->
     <script src="../minible/js/pages/sweet-alerts.init.js"></script>
+    <script src="../minible/js/custom.js"></script>
+    <script>
+        function copyreff() {
+            var copyText = document.getElementById("reflink");
+            copyText.select();
+            copyText.setSelectionRange(0, 99999)
+            document.execCommand("copy");
+            alert("Copied: " + copyText.value);
+        }
+    </script>
     <div class="preloader-wrapper" id="preloader">
         <div class="col">
             <div class="preloader">
