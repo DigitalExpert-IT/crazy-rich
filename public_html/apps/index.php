4ccf39d1205772d52098a1c8ef3478a911a6694f
Muhammad Rizky Firdaus <62287144+ubudab109@users.noreply.github.com>
2021-04-01 20:56:27 +0700

    Update index.php

--- public_html/apps/index.php
+++ public_html/apps/index.php
@@ -86,6 +86,7 @@ include('template/fungsi.php');
 	<div id="layout-wrapper">
 		<!-- JAVASCRIPT -->
 		<script src="../minible/libs/jquery/jquery.min.js"></script>
+		<script src="../minible/libs/metismenu/metisMenu.min.js"></script>
 
 		<!-- apexcharts
 		<script src="../minible/libs/apexcharts/apexcharts.min.js"></script> -->
@@ -234,7 +235,6 @@ include('template/fungsi.php');
 	<!-- Right bar overlay-->
 	<div class="rightbar-overlay"></div>
 	<script src="../minible/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
-	<script src="../minible/libs/metismenu/metisMenu.min.js"></script>
 	<script src="../minible/libs/simplebar/simplebar.min.js"></script>
 	<script src="../minible/libs/node-waves/waves.min.js"></script>
 	<script src="../minible/libs/waypoints/lib/jquery.waypoints.min.js"></script>
