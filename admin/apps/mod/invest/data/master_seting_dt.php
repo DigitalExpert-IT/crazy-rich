2ad4c61e510efadb70a43db7d39deab96c743a13
Muhammad Rizky Firdaus <62287144+ubudab109@users.noreply.github.com>
2021-04-05 20:48:39 +0700

    Update master_seting_dt.php

--- admin/apps/mod/invest/data/master_seting_dt.php
+++ admin/apps/mod/invest/data/master_seting_dt.php
@@ -109,7 +109,7 @@ $columns = array(
     'dt' => 2,
     'formatter' => function ($d, $row) {
       if ($row['type'] == 0) {
-        $result = dolar($d);
+        $result = '<span>$' . $d . '%</span>';
       } else if ($row['type'] == 1) {
         $result = '<span>' . $d . '%</span>';
       }
