944bec9bdbd3b99e794d69c7216e9a6d4d37608b
Muhammad Rizky Firdaus <62287144+ubudab109@users.noreply.github.com>
2021-04-15 17:30:06 +0700

    Update reg.php

--- public_html/reg.php
+++ public_html/reg.php
@@ -29,9 +29,16 @@ $randomkey = substr($mdun, 25); // if you want sort length code.
 
 $name = mysqli_real_escape_string($con, $_POST['fullname']);
 $refcode = $_POST['reffcode'];
+$passwords = password_hash($_POST['password1'], PASSWORD_DEFAULT);
 
-
-
+if ($refcode != '') {
+  $quref = "select * from users where reff_code='$_POST[reffcode]'";
+  $rsfref = mysqli_query($con, $quref);
+  $rwfef = mysqli_fetch_array($rsfref);
+  $idref = $rwfef['user_id'];
+} else {
+  $idref = 0;
+}
 if (!empty($_POST['email'])) {
   $email = mysqli_real_escape_string($con, $_POST['email']);
   // cek email exist
@@ -44,47 +51,72 @@ if (!empty($_POST['email'])) {
     window.location = "http://crazyrich.trade/register.php";
     </script>';
   } else {
-    if ($refcode != '') {
-      $quref = "select * from users where reff_code='$_POST[reffcode]'";
-      $rsfref = mysqli_query($con, $quref);
-      $rwfef = mysqli_fetch_array($rsfref);
-      $idref = $rwfef['user_id'];
-    } else {
-      $idref = 0;
-    }
-
-    $passwords = password_hash($_POST['password1'], PASSWORD_DEFAULT);
-
     $quadd = "INSERT into users set reff_id='$idref',nama='$name',phone='$_POST[phone]',email_user='$email',password='$passwords',verify_code='$mdun',reff_code='$randomkey',status='1',date_join='$time_now'";
-
-
-    $regist = mysqli_query($con, $quadd);
-    if ($regist) {
-      echo "<script>alert('Register Success'); window.location.href = 'index.php';</script>";
-    } else {
-      echo "<script>alert('Register Fail, Please Try Again'); window.location.href = 'index.php';</script>";
-    }
   }
 } else {
-  $email = null;
-  if ($refcode != '') {
-    $quref = "select * from users where reff_code='$_POST[reffcode]'";
-    $rsfref = mysqli_query($con, $quref);
-    $rwfef = mysqli_fetch_array($rsfref);
-    $idref = $rwfef['user_id'];
-  } else {
-    $idref = 0;
-  }
-
-  $passwords = password_hash($_POST['password1'], PASSWORD_DEFAULT);
-
-  $quadd = "INSERT into users set reff_id='$idref',nama='$name',phone='$_POST[phone]',email_user='$email',password='$passwords',verify_code='$mdun',reff_code='$randomkey',status='1',date_join='$time_now'";
-
-
-  $regist = mysqli_query($con, $quadd);
-  if ($regist) {
-    echo "<script>alert('Register Success'); window.location.href = 'index.php';</script>";
-  } else {
-    echo "<script>alert('Register Fail, Please Try Again'); window.location.href = 'index.php';</script>";
-  }
+  $quadd = "INSERT into users set reff_id='$idref',nama='$name',phone='$_POST[phone]',email_user=null,password='$passwords',verify_code='$mdun',reff_code='$randomkey',status='1',date_join='$time_now'";
 }
+
+$regist = mysqli_query($con, $quadd);
+if ($regist) {
+  echo "<script>alert('Register Success'); window.location.href = 'index.php';</script>";
+} else {
+  echo "<script>alert('Register Fail, Please Try Again'); window.location.href = 'index.php';</script>";
+}
+
+// if (!empty($_POST['email'])) {
+//   $email = mysqli_real_escape_string($con, $_POST['email']);
+//   // cek email exist
+//   $qemailexist = "SELECT email_user FROM users where email_user='$email'";
+//   $proemailexist = mysqli_query($con, $qemailexist);
+//   $resemailexist = mysqli_fetch_array($proemailexist);
+//   if ($resemailexist[0] != NULL) {
+//     echo '<script>
+//     alert("This Email is Exist");
+//     window.location = "http://crazyrich.trade/register.php";
+//     </script>';
+//   } else {
+//     if ($refcode != '') {
+//       $quref = "select * from users where reff_code='$_POST[reffcode]'";
+//       $rsfref = mysqli_query($con, $quref);
+//       $rwfef = mysqli_fetch_array($rsfref);
+//       $idref = $rwfef['user_id'];
+//     } else {
+//       $idref = 0;
+//     }
+
+//     $passwords = password_hash($_POST['password1'], PASSWORD_DEFAULT);
+
+//     $quadd = "INSERT into users set reff_id='$idref',nama='$name',phone='$_POST[phone]',email_user='$email',password='$passwords',verify_code='$mdun',reff_code='$randomkey',status='1',date_join='$time_now'";
+
+
+//     $regist = mysqli_query($con, $quadd);
+//     if ($regist) {
+//       echo "<script>alert('Register Success'); window.location.href = 'index.php';</script>";
+//     } else {
+//       echo "<script>alert('Register Fail, Please Try Again'); window.location.href = 'index.php';</script>";
+//     }
+//   }
+// } else {
+//   $email = null;
+//   if ($refcode != '') {
+//     $quref = "select * from users where reff_code='$_POST[reffcode]'";
+//     $rsfref = mysqli_query($con, $quref);
+//     $rwfef = mysqli_fetch_array($rsfref);
+//     $idref = $rwfef['user_id'];
+//   } else {
+//     $idref = 0;
+//   }
+
+//   $passwords = password_hash($_POST['password1'], PASSWORD_DEFAULT);
+
+//   $quadd = "INSERT into users set reff_id='$idref',nama='$name',phone='$_POST[phone]',email_user='$email',password='$passwords',verify_code='$mdun',reff_code='$randomkey',status='1',date_join='$time_now'";
+
+
+//   $regist = mysqli_query($con, $quadd);
+//   if ($regist) {
+//     echo "<script>alert('Register Success'); window.location.href = 'index.php';</script>";
+//   } else {
+//     echo "<script>alert('Register Fail, Please Try Again'); window.location.href = 'index.php';</script>";
+//   }
+// }
