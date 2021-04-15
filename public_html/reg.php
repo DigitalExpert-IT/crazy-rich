9a8cb7ab21bcb75f784619b05e34b1f192214ecb
Muhammad Rizky Firdaus <62287144+ubudab109@users.noreply.github.com>
2021-04-15 17:48:31 +0700

    Update reg.php

--- public_html/reg.php
+++ public_html/reg.php
@@ -1,7 +1,22 @@
 <?php
 include('assets/dbconnect.php');
-ini_set('display_errors', 1);
+include('assets/PHPMailer/class.phpmailer.php');
+include('assets/PHPMailer/class.smtp.php');
+include('assets/PHPMailer/PHPMailerAutoload.php');
 
+
+// Build POST request:
+/*  $recaptcha_url = 'https://www.google.com/recaptcha/api/siteverify';
+    $recaptcha_secret = '6LfpVMgUAAAAAGlJ38SzEnWxBFwYI_rmd0SojIdC';
+    $recaptcha_response = $_POST['g-recaptcha-response'];
+
+    // Make and decode POST request:
+    $recaptcha = file_get_contents($recaptcha_url . '?secret=' . $recaptcha_secret . '&response=' . $recaptcha_response);
+    $recaptcha = json_decode($recaptcha);
+
+    // Take action based on the score returned:
+    if ($recaptcha->success == 1) {
+*/
 
 // set default time asia/jakarta
 date_default_timezone_set("Asia/Jakarta");
@@ -20,10 +35,9 @@ $dmtun = $dmt . $un;
 $mdun = md5($dmtran . $un);
 $randomkey = substr($mdun, 25); // if you want sort length code.
 
-$name =  $_POST['fullname'];
-$refcode = $_POST['reffcode'];
+$name = mysqli_real_escape_string($con, $_POST['fullname']);
 $passwords = password_hash($_POST['password1'], PASSWORD_DEFAULT);
-
+$refcode = $_POST['reffcode'];
 if ($refcode != '') {
   $quref = "select * from users where reff_code='$_POST[reffcode]'";
   $rsfref = mysqli_query($con, $quref);
@@ -32,21 +46,33 @@ if ($refcode != '') {
 } else {
   $idref = 0;
 }
-if ($_POST['email'] != '') {
-  $email = mysqli_real_escape_string($con, $_POST['email']);
-  $mail = $_POST['email'];
+if (!empty($_POST['email'])) {
+  $quadd = "INSERT into users set reff_id='$idref',nama='$name',phone='$_POST[phone]',email_user='$_POST[email]',password='$passwords',verify_code='$mdun',reff_code='$randomkey',status='1',date_join='$time_now'";
 } else {
-  $email = mysqli_real_escape_string($con, '');
-  $mail = null;
+  $email = null;
+  $quadd = "INSERT into users set reff_id='$idref',nama='$name',phone='$_POST[phone]',email_user='$email',password='$passwords',verify_code='$mdun',reff_code='$randomkey',status='1',date_join='$time_now'";
 }
-$quadd = "INSERT into users set reff_id='$idref',nama='$name',phone='$_POST[phone]',email_user='$mail',password='$passwords',verify_code='$mdun',reff_code='$randomkey',status='1',date_join='$time_now'";
 
 $regist = mysqli_query($con, $quadd);
-echo $quadd;
-echo '<br>';
-echo $regist;
 if ($regist) {
   echo "<script>alert('Register Success'); window.location.href = 'index.php';</script>";
 } else {
   echo "<script>alert('Register Fail, Please Try Again'); window.location.href = 'index.php';</script>";
 }
+// // cek email exist
+// $qemailexist = "SELECT email_user FROM users where email_user='$email'";
+// $proemailexist = mysqli_query($con, $qemailexist);
+// $resemailexist = mysqli_fetch_array($proemailexist);
+// if ($resemailexist[0] != NULL) {
+//   echo '<script>
+//   alert("This Email is Exist");
+//   window.location = "http://crazyrich.trade/register.php";
+//   </script>';
+// } else {
+
+
+
+//   $quadd = "INSERT into users set reff_id='$idref',nama='$name',phone='$_POST[phone]',email_user='$_POST[email]',password='$passwords',verify_code='$mdun',reff_code='$randomkey',status='1',date_join='$time_now'";
+
+ 
+// }
