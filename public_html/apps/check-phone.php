846011f46cb9d0ee11814f960444501f0a685f3d
Muhammad Rizky Firdaus <62287144+ubudab109@users.noreply.github.com>
2021-04-15 18:50:00 +0700

    Update check-phone.php

--- public_html/apps/check-phone.php
+++ public_html/apps/check-phone.php
@@ -1,9 +1,7 @@
 <?php
 
-include('http://putin.smarttrade.top/assets/dbconnect.php');
-if (isset($_POST["phone"])) {
-    $phone = mysqli_real_escape_string($con, $_POST["phone"]);
-    $query = "SELECT * FROM users WHERE phone = '" . $phone . "'";
-    $result = mysqli_query($con, $query);
-    echo mysqli_num_rows($result);
-}
+include('../assets/dbconnect.php');
+$phone = mysqli_real_escape_string($con, $_POST["phone"]);
+$query = "SELECT * FROM users WHERE phone = '" . $phone . "'";
+$result = mysqli_query($con, $query);
+echo mysqli_num_rows($result);
