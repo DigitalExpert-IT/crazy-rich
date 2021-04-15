cc8d74febee0ca82907f2ec7155eb8c7aa81ecc5
abrahamSN <abrahamsn.mayer@gmail.com>
2021-04-15 18:35:52 +0700

    change profile

--- public_html/apps/mod/profile/index.php
+++ public_html/apps/mod/profile/index.php
@@ -5,7 +5,7 @@
             <div class="col-xl-12">
                 <div class="card">
                     <div class="card-body">
-                        <h4 class="card-title">Transaction History</h4>
+                        <h4 class="card-title">Profile</h4>
 
                         <!-- Nav tabs -->
                         <ul class="nav nav-pills nav-justified bg-light" role="tablist">
@@ -15,12 +15,12 @@
                                     <span class="d-none d-sm-block">Change Password</span>
                                 </a>
                             </li>
-                            <li class="nav-item waves-effect waves-light">
-                                <a class="nav-link" data-bs-toggle="tab" href="#navpills2-profile" role="tab">
-                                    <span class="d-block d-sm-none"><i class="fas fa-user-cog"></i></span>
-                                    <span class="d-none d-sm-block">Profile</span>
-                                </a>
-                            </li>
+<!--                            <li class="nav-item waves-effect waves-light">-->
+<!--                                <a class="nav-link" data-bs-toggle="tab" href="#navpills2-profile" role="tab">-->
+<!--                                    <span class="d-block d-sm-none"><i class="fas fa-user-cog"></i></span>-->
+<!--                                    <span class="d-none d-sm-block">Profile</span>-->
+<!--                                </a>-->
+<!--                            </li>-->
                             <!-- <li class="nav-item waves-effect waves-light">
                                 <a class="nav-link" data-bs-toggle="tab" href="#navpills2-referral" role="tab">
                                     <span class="d-block d-sm-none"><i class="fas fa-user-friends"></i></span>
@@ -40,20 +40,20 @@
                             <div class="tab-pane active" id="navpills2-change-password" role="tabpanel">
                                 <?php include('component/change-password.php') ?>
                             </div>
-                            <div class="tab-pane" id="navpills2-profile" role="tabpanel">
-                                <div class="col-md-12 col-xl-6">
-                                    <div class="card">
-                                        <div class="card-body">
-                                            <div>
-                                                <h4 class="mb-1 mt-1">Contact Admin If You Want To Update Your Profile</h4>
-                                                <p class="text-muted mb-0">crazyrich@gmail.com</p>
-                                            </div>
-
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div> <!-- end col-->
-                            </div>
+<!--                            <div class="tab-pane" id="navpills2-profile" role="tabpanel">-->
+<!--                                <div class="col-md-12 col-xl-6">-->
+<!--                                    <div class="card">-->
+<!--                                        <div class="card-body">-->
+<!--                                            <div>-->
+<!--                                                <h4 class="mb-1 mt-1">Contact Admin If You Want To Update Your Profile</h4>-->
+<!--                                                <p class="text-muted mb-0">crazyrich@gmail.com</p>-->
+<!--                                            </div>-->
+<!---->
+<!--                                            </p>-->
+<!--                                        </div>-->
+<!--                                    </div>-->
+<!--                                </div> <!-- end col-->-->
+<!--                            </div>-->
                             <!-- <div class="tab-pane" id="navpills2-referral" role="tabpanel">
 
                             </div>
