a5cb02df84d0f10e5c8562845a45e09d8e368ae1
Muhammad Rizky Firdaus <62287144+ubudab109@users.noreply.github.com>
2021-04-15 17:37:58 +0700

    Update reg.php

--- public_html/reg.php
+++ public_html/reg.php
@@ -8,11 +8,23 @@ date_default_timezone_set("Asia/Jakarta");
 $time = time();
 $time_now = date("Y-m-d H:i:s", $time);
 
+$d = date("d");
+$m = date("m");
+$y = date("Y");
+$t = time();
+$dmt = $d + $m + $y + $t;
+$ran = rand(0, 10000000);
+$dmtran = $dmt + $ran;
+$un =  uniqid();
+$dmtun = $dmt . $un;
+$mdun = md5($dmtran . $un);
+$randomkey = substr($mdun, 25); // if you want sort length code.
+
 $name = mysqli_real_escape_string($con, $_POST['fullname']);
 $refcode = $_POST['reffcode'];
 $passwords = password_hash($_POST['password1'], PASSWORD_DEFAULT);
 
-if (!empty($refcode)) {
+if ($refcode != '') {
   $quref = "select * from users where reff_code='$_POST[reffcode]'";
   $rsfref = mysqli_query($con, $quref);
   $rwfef = mysqli_fetch_array($rsfref);
