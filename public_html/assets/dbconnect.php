7366a7d6504a58c71f5619dbf7f1e1673635dea0
abrahamSN <abrahamsn.mayer@gmail.com>
2021-04-15 18:37:56 +0700

    berubah

--- public_html/assets/dbconnect.php
+++ public_html/assets/dbconnect.php
@@ -3,7 +3,7 @@ ini_set('display_errors', 0);
 
 $userdb = 'root';
 $host = 'localhost';
-$passdb = '12qwwq21';
+$passdb = 'Superman88';
 $dbselect = 'db_mining';
 
 $baseurl = "http://smarttrade.top";
