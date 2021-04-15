5d75ef50e4639d2906e9e2901572c1d2e44ce5c2
Muhammad Rizky Firdaus <62287144+ubudab109@users.noreply.github.com>
2021-04-15 20:45:51 +0700

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -50,14 +50,17 @@
 
 <body>
 
+	<!-- GLOABAL LOADER -->
+	<div id="global-loader" style="margin-top:100px">
+		<div class="d-flex justify-content-center">
+			<div class="spinner-border" role="status">
+				<span class="sr-only">Loading...</span>
+			</div>
+		</div>
+	</div>
+	<!-- /GLOABAL LOADER -->
 	<!-- BACKGROUND-IMAGE -->
 	<div class="login-img">
-
-		<!-- GLOABAL LOADER -->
-		<div id="global-loader">
-			<div class="lds-hourglass" style="margin-top: 275px;"></div>
-		</div>
-		<!-- /GLOABAL LOADER -->
 
 		<!-- PAGE -->
 		<div class="page">
