17e170e32cbaaeddfb7dbe832e9f302a4839cb21
Muhammad Rizky Firdaus <62287144+ubudab109@users.noreply.github.com>
2021-04-15 18:55:52 +0700

    Update index.php

--- public_html/apps/index.php
+++ public_html/apps/index.php
@@ -58,25 +58,9 @@ include('template/fungsi.php');
 <body>
 
 	<div class="preloader-wrapper" id="preloader">
-		<div class="col">
+		<div class="col-xl-12">
 			<div class="preloader">
-
-				<div class="spinner-grow text-primary m-1" role="status">
-					<span class="sr-only">Loading...</span>
-				</div>
-				<div class="spinner-grow text-secondary m-1" role="status">
-					<span class="sr-only">Loading...</span>
-				</div>
-				<div class="spinner-grow text-success m-1" role="status">
-					<span class="sr-only">Loading...</span>
-				</div>
-				<div class="spinner-grow text-info m-1" role="status">
-					<span class="sr-only">Loading...</span>
-				</div>
-				<div class="spinner-grow text-warning m-1" role="status">
-					<span class="sr-only">Loading...</span>
-				</div>
-				<div class="spinner-grow text-danger m-1" role="status">
+				<div class="spinner-border text-primary m-1" role="status">
 					<span class="sr-only">Loading...</span>
 				</div>
 			</div>
