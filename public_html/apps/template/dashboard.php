fca85259d55f74c8c5be70e53f277c84bc3ac081
Muhammad Rizky Firdaus <62287144+ubudab109@users.noreply.github.com>
2021-04-20 01:47:04 +0700

    Update dashboard.php

--- public_html/apps/template/dashboard.php
+++ public_html/apps/template/dashboard.php
@@ -53,18 +53,23 @@ $i = 0;
                             <i class="mdi mdi-account-group me-10 icon-card icon-blue"></i>
                         </div>
                         <?php
-                        $queryLevel2 = "SELECT * FROM users WHERE reff_id = $_SESSION[user_id]";
+                        $queryLevel = "SELECT * FROM users WHERE reff_id = $_SESSION[user_id]";
+                        $resLevel = mysqli_query($con, $queryLevel2);
+                        $getLevel = mysqli_fetch_array($resLevel2);
+                        $idLevel = $getLevel2['user_id'];
+
+
+
+                        $queryLevel2 = "SELECT * FROM users WHERE reff_id = $idLevel";
                         $resLevel2 = mysqli_query($con, $queryLevel2);
                         $getLevel2 = mysqli_fetch_array($resLevel2);
                         $countLvl2 = mysqli_num_rows($resLevel2);
                         $idLevel2 = $getLevel2['user_id'];
 
-
                         $queryLevel3 = "SELECT * FROM users WHERE reff_id = $idLevel2";
                         $resLevel3 = mysqli_query($con, $queryLevel3);
                         $getLevel3 = mysqli_fetch_array($resLevel3);
                         $countLvl3 = mysqli_num_rows($resLevel3);
-                        $idLevel3 = $getLevel3['user_id'];
                         ?>
                         <div>
                             <h4 class="mb-1 mt-1"><span data-plugin="counterup"><?= $countLvl2 ?></span></h4>
