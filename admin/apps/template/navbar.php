a417f04025ec6fb21039326fa6985c1cd2609692
Muhammad Rizky Firdaus <62287144+ubudab109@users.noreply.github.com>
2021-04-20 23:09:30 +0700

    Update navbar.php

--- admin/apps/template/navbar.php
+++ admin/apps/template/navbar.php
@@ -1,9 +1,3 @@
-<?php
-$qurateidr = "select value from master_seting where nama_seting='rate_idr_to_usd'";
-$rsrate = mysqli_query($con, $qurateidr);
-$rwrate = mysqli_fetch_array($rsrate);
-$rateidr = $rwrate['value'];
-?>
 <style>
     @media all and (max-width: 699px) and (min-width: 520px),
     (min-width: 1151px) {
@@ -15,14 +9,14 @@ $rateidr = $rwrate['value'];
 <div class="navbar-header">
     <div class="d-flex">
         <!-- LOGO -->
+
         <div class="navbar-brand-box">
             <a href="index.php" class="logo logo-dark">
                 <span class="logo-sm">
                     <img src="../images/logo/logo_round_sm.png" alt="" height="30">
-
                 </span>
                 <span class="logo-lg">
-                    <img src="../images/logo/logo_sm.png" alt="" height="50">
+                    <img src="../images/logo/logo_round_sm.png" alt="" height="50">
                 </span>
             </a>
 
@@ -31,11 +25,9 @@ $rateidr = $rwrate['value'];
                     <img src="../images/logo/logo_round_sm.png" alt="" height="30">
                 </span>
                 <span class="logo-lg">
-                    <img src="../images/logo/logo_sm.png" alt="" height="50">
+                    <img src="../images/logo/logo_round_sm.png" alt="" height="50">
                 </span>
             </a>
-
-
         </div>
 
         <button type="button" class="btn btn-sm px-3 font-size-16 header-item waves-effect vertical-menu-btn">
@@ -55,27 +47,20 @@ $rateidr = $rwrate['value'];
         <div class="dropdown d-inline-block">
 
             <button type="button" class="btn header-item waves-effect" id="page-header-user-dropdown" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <span class="d-none d-xl-inline-block ms-1 fw-medium font-size-15"><?= $_SESSION['nama'] ?></span>
+                <span class="d-none d-xl-inline-block ms-1 fw-medium font-size-15">Admin</span>
                 <i class="uil-angle-down d-none d-xl-inline-block font-size-15"></i>
             </button>
 
             <div class="dropdown-menu dropdown-menu-end">
                 <!-- item-->
                 <!-- <a class="dropdown-item" href="#"><i class="uil uil-user-circle font-size-18 align-middle text-muted me-1"></i> <span class="align-middle">View Profile</span></a> -->
-                <span class="dropdown-item"><i class="uil uil-wallet font-size-18 align-middle me-1 text-muted"></i> <span class="align-middle">Balance: <?= dolar(saldo($_SESSION['user_id'])) ?> (<?= rupiah(saldo($_SESSION['user_id']) * $rateidr) ?>)</span></span>
-                <a class="dropdown-item d-block" href="?mod=profile&cmd=index"><i class="uil uil-cog font-size-18 align-middle me-1 text-muted"></i> <span class="align-middle">Settings</span> <span class="badge bg-soft-success rounded-pill mt-1 ms-2"></span></a>
+                <a class="dropdown-item d-block" href="?mod=settings&cmd=setting"><i class="uil uil-cog font-size-18 align-middle me-1 text-muted"></i> <span class="align-middle">Settings</span> <span class="badge bg-soft-success rounded-pill mt-1 ms-2"></span></a>
                 <!-- <a class="dropdown-item" href="#"><i class="uil uil-lock-alt font-size-18 align-middle me-1 text-muted"></i> <span class="align-middle">Lock screen</span></a> -->
                 <a class="dropdown-item" href="logout.php"><i class="uil uil-sign-out-alt font-size-18 align-middle me-1 text-muted"></i> <span class="align-middle">Sign out</span></a>
             </div>
-
         </div>
 
         <div class="" id="logout-sm">
-            <?php
-            $name = $_SESSION['nama'];
-            $username = explode(' ', $name);
-            ?>
-            <?= $username[0] ?>
             <a href="logout.php">
                 <button data-bs-toggle="signout" type="button" class="btn header-item noti-icon waves-effect">
                     <i class="uil uil-sign-out-alt"></i>
