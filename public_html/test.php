61205462dc1270ebc05a24848d1870d8e9dac052
Muhammad Rizky Firdaus <62287144+ubudab109@users.noreply.github.com>
2021-04-20 11:45:11 +0700

    Update test.php

--- public_html/test.php
+++ public_html/test.php
@@ -1,6 +1,7 @@
 <?php
 include('assets/dbconnect.php');
 session_start();
+ini_set('display_errors', 1);
 
 
 $queryLevel = "SELECT * FROM users WHERE reff_id = $_SESSION[user_id]";
@@ -8,17 +9,20 @@ $resLevel = mysqli_query($con, $queryLevel2);
 $getLevel = mysqli_fetch_row($resLevel2);
 $idLevel = $getLevel[0];
 
+// echo $idLevel;
+
+
+
+
+// $queryLevel2 = "SELECT * FROM users WHERE reff_id = $idLevel";
+// $resLevel2 = mysqli_query($con, $queryLevel2);
+// $countLvl2 = mysqli_fetch_array($resLevel2);
+// $idLevel2 = $countLvl2['user_id'];
+
+
+// $queryLevel3 = "SELECT COUNT(*) as reff_3 FROM users WHERE reff_id = $idLevel2";
+// $resLevel3 = mysqli_query($con, $queryLevel3);
+// $countLvl3 = mysqli_fetch_array($resLevel3);
+
+
 echo $idLevel;
-
-
-
-
-$queryLevel2 = "SELECT * FROM users WHERE reff_id = $idLevel";
-$resLevel2 = mysqli_query($con, $queryLevel2);
-$countLvl2 = mysqli_fetch_array($resLevel2);
-$idLevel2 = $countLvl2['user_id'];
-
-
-$queryLevel3 = "SELECT COUNT(*) as reff_3 FROM users WHERE reff_id = $idLevel2";
-$resLevel3 = mysqli_query($con, $queryLevel3);
-$countLvl3 = mysqli_fetch_array($resLevel3);
