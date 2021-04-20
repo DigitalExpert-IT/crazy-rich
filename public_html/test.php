1294ee578dd5901345a9848b310b12768af3486a
Muhammad Rizky Firdaus <62287144+ubudab109@users.noreply.github.com>
2021-04-20 11:56:06 +0700

    Update test.php

--- public_html/test.php
+++ public_html/test.php
@@ -1,6 +1,5 @@
 <?php
 include('assets/dbconnect.php');
-include('ap');
 session_start();
 ini_set('display_errors', 1);
 
@@ -16,16 +15,23 @@ $idLevel = $getLevel[0];
 
 
 $queryLevel2 = "SELECT COUNT(*) as reff_2 FROM users WHERE reff_id = $idLevel";
+$queryLevel2F = "SELECT * FROM users WHERE reff_id = $idLevel";
+
 $resLevel2 = mysqli_query($con, $queryLevel2);
 $countLvl2 = mysqli_fetch_array($resLevel2);
+$resLvl2f = mysqli_query($con, $queryLevel2F);
+$getLvl2f = mysqli_fetch_row($resLvl2f);
+
 $idLevel2 = $countLvl2['reff_2'];
+$idReff2 = $getLvl2f[0];
 
-
-// $queryLevel3 = "SELECT COUNT(*) as reff_3 FROM users WHERE reff_id = $idLevel2";
-// $resLevel3 = mysqli_query($con, $queryLevel3);
-// $countLvl3 = mysqli_fetch_array($resLevel3);
+$queryLevel3 = "SELECT COUNT(*) as reff_3 FROM users WHERE reff_id = $idReff2";
+$resLevel3 = mysqli_query($con, $queryLevel3);
+$countLvl3 = mysqli_fetch_array($resLevel3);
 
 
 echo $idLevel;
 echo '<br>';
 echo $idLevel2;
+echo '<br>';
+echo $countLvl3['reff_3'];
