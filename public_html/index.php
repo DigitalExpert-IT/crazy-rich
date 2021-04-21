09764a468210c7cb195b5bf32fb39f66a45f9ead
Muhammad Rizky Firdaus <62287144+ubudab109@users.noreply.github.com>
2021-04-21 17:36:05 +0700

    Update index.php

--- public_html/index.php
+++ public_html/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <!-- For IE -->
@@ -30,102 +31,108 @@
 </head>
 
 <body>
-<div class="main-page-wrapper">
+    <div class="main-page-wrapper">
 
 
-    <!--
+        <!--
     =============================================
         Cryonik Header
     ==============================================
     -->
-    <div class="Cryonik-header">
-        <div class="container">
-            <div class="main-header-wrapper clearfix">
-                <div class="logo"><a href="index.html"><img src="images/logo/logo.png" alt=""></a></div>
+        <div class="Cryonik-header">
+            <div class="container">
+                <div class="main-header-wrapper clearfix">
+                    <div class="logo"><a href="index.html"><img src="images/logo/logo.png" alt=""></a></div>
 
-                <!-- ================= Theme Menu Wrapper =================== -->
-                <div class="navbar navbar-expand-lg bsnav bsnav-sticky-slide float-right">
-                    <button class="navbar-toggler toggler-spring"><span class="navbar-toggler-icon"></span></button>
-                    <div class="collapse navbar-collapse">
-                        <ul class="navbar-nav navbar-mobile mr-0">
-                            <li class="header-widget">
-                                <ul class="clearfix">
-                                    <li class="login-button"><a href="login.php">Log in</a></li>
-                                    <li class="invest"><a href="register.php" class="grdn-bg">Start Investing</a></li>
+                    <!-- ================= Theme Menu Wrapper =================== -->
+                    <div class="navbar navbar-expand-lg bsnav bsnav-sticky-slide float-right">
+                        <button class="navbar-toggler toggler-spring"><span class="navbar-toggler-icon"></span></button>
+                        <div class="collapse navbar-collapse">
+                            <ul class="navbar-nav navbar-mobile mr-0">
+                                <li class="header-widget">
+                                    <ul class="clearfix">
+                                        <li class="login-button"><a href="login.php">Log in</a></li>
+                                        <li class="invest"><a href="register.php" class="grdn-bg">Start Investing</a></li>
 
-                                </ul>
-                            </li> <!-- /.header-widget -->
-                        </ul> <!-- /.navbar-nav -->
-                    </div> <!-- /.navbar-collapse -->
-                </div> <!-- /.bsnav -->
-                <div id="login-md"><a href="login.php">Log in</a></div>
-            </div> <!-- /.main-header-wrapper -->
-            <div class="bsnav-mobile">
-                <div class="bsnav-mobile-overlay"></div>
-                <div class="navbar"></div>
-            </div>
-        </div> <!-- /.containe -->
-    </div> <!-- /.Cryonik-header -->
+                                    </ul>
+                                </li> <!-- /.header-widget -->
+                            </ul> <!-- /.navbar-nav -->
+                        </div> <!-- /.navbar-collapse -->
+                    </div> <!-- /.bsnav -->
+                    <div id="login-md"><a href="login.php">Log in</a></div>
+                </div> <!-- /.main-header-wrapper -->
+                <div class="bsnav-mobile">
+                    <div class="bsnav-mobile-overlay"></div>
+                    <div class="navbar"></div>
+                </div>
+            </div> <!-- /.containe -->
+        </div> <!-- /.Cryonik-header -->
 
 
 
-    <!--
+        <!--
     =============================================
         Cryonik Main Banner
     ==============================================
     -->
-    <div class="Cryonik-main-banner">
-        <div class="img-wrapper float-right fadeInRight wow" data-wow-duration="1.2s" data-wow-delay="1.6s"><img src="images/home/intro-image.png" alt=""></div>
-        <div class="container">
-            <div class="main-wrapper clearfix">
-                <div class="text-wrapper float-left">
-                    <h1 class="main-title fadeInDown wow" data-wow-duration="1.2s" data-wow-delay="0.5s">Your Best Smart Contract Trading</h1>
-                    <p class="sub-text fadeInUp wow" data-wow-duration="1.2s" data-wow-delay="0.5s">The best investment for your trading online business.</p>
+        <div class="Cryonik-main-banner">
+            <div class="img-wrapper float-right fadeInRight wow" data-wow-duration="1.2s" data-wow-delay="1.6s"><img src="images/home/intro-image.png" alt=""></div>
+            <div class="container">
+                <div class="main-wrapper clearfix">
+                    <div class="text-wrapper float-left">
+                        <h1 class="main-title fadeInDown wow" data-wow-duration="1.2s" data-wow-delay="0.5s">Your Best Smart Contract Trading</h1>
+                        <p class="sub-text fadeInUp wow" data-wow-duration="1.2s" data-wow-delay="0.5s">The best investment for your trading online business.</p>
 
-                    <ul class="button-group fadeInUp wow" data-wow-duration="1.2s" data-wow-delay="1s">
-                        <li class="start-button"><a href="register.php" class="theme-button grdn-bg">Register Now</a></li>
-                        <li class="start-button"><a href="login.php" class="theme-button grdn-bg">Login</a></li>
-                    </ul>
-                    <div class="icon-text fadeInUp wow" data-wow-duration="1.2s">
-                        <img src="images/icon/2.png" alt="" class="img-icon">
-                        <p>Your investment is covered by our <br> protection policy. </p>
-                    </div>
-                </div> <!-- /.text-wrapper -->
-            </div> <!-- /.main-wrapper -->
-        </div> <!-- /.container -->
-    </div> <!-- /.Cryonik-main-banner -->
+                        <ul class="button-group fadeInUp wow" data-wow-duration="1.2s" data-wow-delay="1s">
+                            <li class="start-button"><a href="register.php" class="theme-button grdn-bg">Register Now</a></li>
+                            <li class="start-button"><a href="login.php" class="theme-button grdn-bg">Login</a></li>
+                        </ul>
+                        <div class="icon-text fadeInUp wow" data-wow-duration="1.2s">
+                            <img src="images/icon/2.png" alt="" class="img-icon">
+                            <p>Your investment is covered by our <br> protection policy. </p>
+                        </div>
+                    </div> <!-- /.text-wrapper -->
+                </div> <!-- /.main-wrapper -->
+            </div> <!-- /.container -->
+        </div> <!-- /.Cryonik-main-banner -->
 
 
 
-    <!-- Optional JavaScript _____________________________  -->
+        <!-- Optional JavaScript _____________________________  -->
 
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <!-- jQuery -->
-    <script src="vendor/jquery.2.2.3.min.js"></script>
-    <!-- Popper js -->
-    <script src="vendor/popper.js/popper.min.js"></script>
-    <!-- Bootstrap JS -->
-    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
-    <!-- menu  -->
-    <script src="vendor/bsnav-master/bsnav.min.js"></script>
-    <!-- Dropdown Selector  -->
-    <script src="vendor/chosen/chosen.jquery.min.js"></script>
-    <!-- Font-awesome -->
-    <script src="fonts/fontawesome/js/all.min.js"></script>
-    <!-- Range Slider -->
-    <script src="vendor/ion.rangeSlider/ion.rangeSlider.js"></script>
-    <!-- WOW js -->
-    <script src="vendor/WOW-master/dist/wow.min.js"></script>
-    <!-- owl.carousel -->
-    <script src="vendor/owl-carousel/owl.carousel.min.js"></script>
-    <!-- js count to -->
-    <script src="vendor/jquery.appear.js"></script>
-    <script src="vendor/jquery.countTo.js"></script>
-    <!-- Fancybox -->
-    <script src="vendor/fancybox/dist/jquery.fancybox.min.js"></script>
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <!-- jQuery -->
+        <script src="vendor/jquery.2.2.3.min.js"></script>
+        <!-- Popper js -->
+        <script src="vendor/popper.js/popper.min.js"></script>
+        <!-- Bootstrap JS -->
+        <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+        <!-- menu  -->
+        <script src="vendor/bsnav-master/bsnav.min.js"></script>
+        <!-- Dropdown Selector  -->
+        <script src="vendor/chosen/chosen.jquery.min.js"></script>
+        <!-- Font-awesome -->
+        <script src="fonts/fontawesome/js/all.min.js"></script>
+        <!-- Range Slider -->
+        <script src="vendor/ion.rangeSlider/ion.rangeSlider.js"></script>
+        <!-- WOW js -->
+        <script src="vendor/WOW-master/dist/wow.min.js"></script>
+        <!-- owl.carousel -->
+        <script src="vendor/owl-carousel/owl.carousel.min.js"></script>
+        <!-- js count to -->
+        <script src="vendor/jquery.appear.js"></script>
+        <script src="vendor/jquery.countTo.js"></script>
+        <!-- Fancybox -->
+        <script src="vendor/fancybox/dist/jquery.fancybox.min.js"></script>
 
-    <!-- Theme js -->
-    <script src="js/theme.js"></script>
-</div> <!-- /.main-page-wrapper -->
+        <!-- Theme js -->
+        <script src="js/theme.js"></script>
+        <script>
+            $(document).ready(function() {
+                sessionStorage.setItem("is_visited", "dark-mode-switch")
+            })
+        </script>
+    </div> <!-- /.main-page-wrapper -->
 </body>
+
 </html>
